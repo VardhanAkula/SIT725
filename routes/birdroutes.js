70b7f9a0aad34b6f4a26a1d2058c24b5d132bce6
VardhanAkula <138274572+VardhanAkula@users.noreply.github.com>
2025-08-27 22:44:22 +1000

    Delete routes directory

--- routes/birdroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('Birds route: list of birds will be shown here.');
-});
-
-module.exports = router;
